df1e38622d29079b802156ef65d43322f3d308ef
ManNjoro <elijohnmwoho@gmail.com>
2026-01-31 19:01:58 +0300

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchRouter} from "./route/matches.js";
 
 const app = express();
 const PORT = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, welcome to the Sportz server!');
 });
+
+app.use('/matches', matchRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
