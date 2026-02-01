3fea3790730ffe6eb7ba7ed02017b0c8bb379e40
ManNjoro <elijohnmwoho@gmail.com>
2026-02-02 02:17:38 +0300

    configure seed file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from 'express';
 import {matchRouter} from "./route/matches.js";
 import http from 'http';
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('Hello, welcome to the Sportz server!');
 });
 
-app.use(securityMiddleware())
+// app.use(securityMiddleware())
 
 app.use('/matches', matchRouter)
 app.use('/matches/:id/commentary', commentaryRouter)
