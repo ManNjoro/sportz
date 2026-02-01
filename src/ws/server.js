1fcfc75ec72823fbd8c2d6e979b645ed9e862667
ManNjoro <elijohnmwoho@gmail.com>
2026-02-01 22:38:57 +0300

    implement coderabbit suggested fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -20,35 +20,33 @@ export function attachWebSocketServer(server){
     });
 
     server.on('upgrade', async (req, socket, head) => {
-        const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+        const {pathname} = new URL(req.url, `http://${req.headers.host}`)
 
-        if (pathname === '/ws') {
-            if (wsArcjet) {
-                try {
-                    const decision = await wsArcjet.protect(req);
-                    if (decision.isDenied()) {
-                        if (decision.reason.isRateLimit()) {
-                            socket.write('HTTP/1.1 429 Too Many Requests\r\n\r\n');
-                        } else {
-                            socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
-                        }
-                        socket.destroy();
-                        return;
+        if (pathname !== '/ws') return;
+        if (wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+                if (decision.isDenied()) {
+                    if (decision.reason.isRateLimit()) {
+                        socket.write('HTTP/1.1 429 Too Many Requests\r\n\r\n');
+                    } else {
+                        socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
                     }
-                } catch (e) {
-                    console.error('Arcjet protection error during upgrade', e);
-                    socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
                     socket.destroy();
                     return;
                 }
+            } catch (e) {
+                console.error('Arcjet protection error during upgrade', e);
+                socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
+                socket.destroy();
+                return;
+            }
             }
 
             wss.handleUpgrade(req, socket, head, (ws) => {
                 wss.emit('connection', ws, req);
             });
-        } else {
-            socket.destroy();
-        }
+
     });
 
     wss.on('connection', (socket) => {
@@ -70,7 +68,10 @@ export function attachWebSocketServer(server){
         });
     }, 30000)
 
-    wss.on('close', () => clearInterval(interval));
+    server.on('close', () => {
+        wss.clients.forEach(ws => ws.terminate());
+        wss.close();
+    });
 
     function broadcastMatchCreated(match){
         broadcast(wss, {type: 'match_created', data: match})
